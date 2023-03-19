a34c1e2117f157373d478e4bd4959dec0bd241ec
Ashwin <94456641+ashwinsathish@users.noreply.github.com>
2023-03-20 00:42:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from sklearn.model_selection import train_test_split
 
 #scaler = pickle.load(open('scaler_model.pkl','rb'))
 model = pickle.load(open('xgb_pred_model.pkl','rb'))
-df2 = pd.read_csv("Mod_acc_data.csv")
+# df2 = pd.read_csv("Mod_acc_data.csv")
 
-X = df2[['Category_Code', 'Accident_Type_Code', 'Year', 'Month']]
-y = df2['Value']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# X = df2[['Category_Code', 'Accident_Type_Code', 'Year', 'Month']]
+# y = df2['Value']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
 
 from flask import Flask, request, jsonify
 
@@ -48,7 +48,7 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 
-def predict_accidents():
+def predict():
 
     category_code = int(request.form.get('category_code'))
     acc_type_code = int(request.form.get('acc_type_code'))
