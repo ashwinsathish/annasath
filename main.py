e2c1127ec436c10f5c15573ff8cefbd9687a1305
Ashwin <94456641+ashwinsathish@users.noreply.github.com>
2023-03-20 02:52:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,48 +23,48 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__, template_folder='./templates')
 
-# @app.route('/')
+@app.route('/')
 
-# def index():
-#     return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-# @app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 
-# def predict():
+def predict():
 
-#     category_code = int(request.form.get('category_code'))
-#     acc_type_code = int(request.form.get('acc_type_code'))
-#     year = int(request.form.get('year'))
-#     month = int(request.form.get('month'))
+    category_code = int(request.form.get('category_code'))
+    acc_type_code = int(request.form.get('acc_type_code'))
+    year = int(request.form.get('year'))
+    month = int(request.form.get('month'))
 
-#     #prediction
-#     input_data = np.array([[category_code, acc_type_code, year, month]])
-#     scaled_data = scaler.transform(input_data)
-#     result = model.predict(scaled_data)
-#     return render_template('index.html',result=result)
-
-@app.route('/predict_acc', methods=['POST'])
-
-def predict_acc():
-    # Parse the JSON request body
-    data = request.get_json()
-
-    # Extract the values for each feature
-    category_code = data['category_code']
-    acc_type_code = data['accident_type_code']
-    year = data['year']
-    month = data['month']
-
-    # Transform the input data
+    #prediction
     input_data = np.array([[category_code, acc_type_code, year, month]])
     scaled_data = scaler.transform(input_data)
+    result = model.predict(scaled_data)
+    return render_template('index.html',result=result)
 
-    # Make a prediction
-    prediction = model.predict(scaled_data)
+# @app.route('/predict_acc', methods=['POST'])
 
-    # Return the prediction as a JSON object
-    response = {'predictions': prediction.tolist()}
-    return jsonify(response)
+# def predict_acc():
+#     # Parse the JSON request body
+#     data = request.get_json()
+
+#     # Extract the values for each feature
+#     category_code = data['category_code']
+#     acc_type_code = data['accident_type_code']
+#     year = data['year']
+#     month = data['month']
+
+#     # Transform the input data
+#     input_data = np.array([[category_code, acc_type_code, year, month]])
+#     scaled_data = scaler.transform(input_data)
+
+#     # Make a prediction
+#     prediction = model.predict(scaled_data)
+
+#     # Return the prediction as a JSON object
+#     response = {'predictions': prediction.tolist()}
+#     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
