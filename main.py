3f26742c382e9f1afee2ff820e41503fa7f3c17b
Ashwin <94456641+ashwinsathish@users.noreply.github.com>
2023-03-20 01:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 #scaler = pickle.load(open('scaler_model.pkl','rb'))
-model = pickle.load(open('xgb_pred_model','rb'))
-# df2 = pd.read_csv("Mod_acc_data.csv")
+model = pickle.load(open('my_model.pkl','rb'))
+df2 = pd.read_csv("Mod_acc_data.csv")
 
-# X = df2[['Category_Code', 'Accident_Type_Code', 'Year', 'Month']]
-# y = df2['Value']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = df2[['Category_Code', 'Accident_Type_Code', 'Year', 'Month']]
+y = df2['Value']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# scaler = StandardScaler()
-# X_train = scaler.fit_transform(X_train)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
 
 from flask import Flask, request, jsonify, render_template
 
@@ -57,8 +57,8 @@ def predict():
 
     #prediction
     input_data = np.array([[category_code, acc_type_code, year, month]])
-    #scaled_data = scaler.transform(input_data)
-    result = model.predict(input_data)
+    scaled_data = scaler.transform(input_data)
+    result = model.predict(scaled_data)
     return render_template('index.html',result=result)
 
 if __name__ == '__main__':
